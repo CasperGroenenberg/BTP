01794dd13e9e8c5631a1e89252eb19c1f4171a10
Casper Groenenberg <c.groenenberg@hotmail.com>
2019-10-10 12:46:30 +0200

    rewind concept

--- player.js
+++ player.js
@@ -1,40 +1,38 @@
-require('./embed');
-
-// jQuery(document).ready(function($) {
-//     $('.twitch-embed').append('<div id="twitch-embed"></div>')
-//     $('.post-text').append('<button id="rewind">rewind</button>')
-//     $('.post-text').append('<button id="live">live</button>')
-
-    const embed = new Twitch.Embed("twitch-embed", {
-        width: "100%",
-        height: "400px",
-        channel: "xqcow",
-        theme: "dark",
-        chat: "mobile",
-        allowfullscreen: "false",
-        layout: "video"
-      });
+require('./embed.js');
+require('./jquery.min.js')
 
 
+(function($) {
+    $(document).ready(function() {
 
-    embed.addEventListener(Twitch.Embed.VIDEO_READY, function() {
-        console.log('The video is ready');
+        const embed = new Twitch.Embed("twitch-embed", {
+            width: "100%",
+            height: "400px",
+            channel: "dansgaming",
+            theme: "dark",
+            chat: "mobile",
+            allowfullscreen: "false",
+            layout: "video"
+            });
+        
+        embed.addEventListener(Twitch.Embed.VIDEO_READY, function() {
+            console.log('The video is ready');
+        });
+
+        
+        
+        $('#rewind').click(function($) {
+            console.log('clicked')
+            let currentTime = embed.getPlayer().getCurrentTime()
+            embed.getPlayer().seek(currentTime-5);
+        });
+        
+        $('#live').click(function($) {
+            let currentTime = embed.getPlayer().getCurrentTime()
+            // embed.getPlayer().seek(currentTime);
+            embed.getPlayer().pause().play();
+        }); 
     });
-
-    console.log('loaded')
-
-    // $('#rewind').click(function($) {
-    //     let currentTime = embed.getPlayer().getCurrentTime()
-    //     embed.getPlayer().seek(currentTime-5);
-    // });
-    
-    // $('#live').click(function($) {
-    //     let currentTime = embed.getPlayer().getCurrentTime()
-    //     // embed.getPlayer().seek(currentTime);
-    //     embed.getPlayer().pause().play();
-
-    // });    
-// });
-
+})(jQuery);
 
 
